96313d4e71300116a7879df575565045f7a109cb
Mishal Mp <mishalmp916@gmail.com>
2024-04-04 02:36:11 +0530

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ sudo mv env .env
 
 sudo apt-get update
 echo "installing python and pip"
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3 python3-pip gunicorn
 
 # Install application dependencies from requirements.txt
 echo "Install application dependencies from requirements.txt"
